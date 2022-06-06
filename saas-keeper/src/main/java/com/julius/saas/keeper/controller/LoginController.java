74914a7abc15553e989eb35926898b6ecf5277e4
Julius <2311922868@qq.com>
2022-06-06 22:50:01 +0800

    DevOps 登录相关

--- saas-keeper/src/main/java/com/julius/saas/keeper/controller/LoginController.java
+++ saas-keeper/src/main/java/com/julius/saas/keeper/controller/LoginController.java
@@ -1,18 +1,23 @@
 package com.julius.saas.keeper.controller;
 
+import com.julius.saas.keeper.service.LoginService;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author julius zhou
  * @date 5/31/22 5:13 PM
  * <p>
- *
+ *      登录控制器
  * </p>
  **/
 @RestController
 public class LoginController {
 
 
+    @Resource
+    private LoginService loginService;
 
 
 
